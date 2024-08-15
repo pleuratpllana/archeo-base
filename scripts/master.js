ae49cb303d59d8dfa25e965b73b085dbc9bf0978
ThePSquared <info@the-p-squared.com>
2024-08-15 12:12:05 +0200

    Initial commit - source files for ArcheoBase website

---
+++ scripts/master.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $(".loginBtn").click(function () {
+        location.href = "https://archeobase.ch";
+    });
+
+    $(document).on('click', '#read_more_scroll', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 200);
+    });
+
+    $(".free_test").click(function () {
+        location.href = contact_page_url;
+    });
+
+
+});
